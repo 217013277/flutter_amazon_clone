4120e135d4883c3924c28408d8f08c4f2d6253a4
yiuyu <217013277@stu.vtc.edu.hk>
2022-09-30 03:37:55 +0800

    create applepay and google pay

--- lib/features/address/screens/address_screen.dart
+++ lib/features/address/screens/address_screen.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_amazon_clone/provider/user_provider.dart';
+import 'package:flutter_amazon_clone/constants/utils.dart';
+import 'package:pay/pay.dart';
 import 'package:provider/provider.dart';
+
+import 'package:flutter_amazon_clone/provider/user_provider.dart';
 
 import '../../../common/widgets/custom_textfield.dart';
 import '../../../constants/global_variables.dart';
 
 class AddressScreen extends StatefulWidget {
   static const String routeName = "/address";
-  const AddressScreen({super.key});
+  final String totalAmount;
+  const AddressScreen({super.key, required this.totalAmount});
 
   @override
   State<AddressScreen> createState() => _AddressScreenState();
@@ -21,6 +25,18 @@ class _AddressScreenState extends State<AddressScreen> {
   final TextEditingController _pincodeController = TextEditingController();
   final TextEditingController _cityController = TextEditingController();
 
+  String addressToBeUsed = "";
+  final List<PaymentItem> _paymentItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _paymentItems.add(PaymentItem(
+        amount: widget.totalAmount,
+        label: "Total",
+        status: PaymentItemStatus.final_price));
+  }
+
   @override
   void dispose() {
     super.dispose();
@@ -30,10 +46,36 @@ class _AddressScreenState extends State<AddressScreen> {
     _cityController.dispose();
   }
 
+  void onApplePayResult(res) {
+    if (Provider.of<UserProvider>(context).user.address.isEmpty) {}
+  }
+
+  void onGooglePayResult(res) {}
+
+  void payPressed(String addressFromProvider) {
+    addressToBeUsed = "";
+    bool isForm = _flatBuildingController.text.isNotEmpty ||
+        _areaController.text.isNotEmpty ||
+        _pincodeController.text.isNotEmpty ||
+        _cityController.text.isNotEmpty;
+
+    if (isForm) {
+      if (_addressFormKey.currentState!.validate()) {
+        addressToBeUsed =
+            "${_flatBuildingController.text}, ${_areaController.text}, ${_cityController.text} - ${_pincodeController.text}";
+      } else {
+        throw Exception('Please enter all the values!');
+      }
+    } else if (addressFromProvider.isNotEmpty) {
+      addressToBeUsed = addressFromProvider;
+    } else {
+      showSnackBar(context, "Error");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // var address = context.watch<UserProvider>().user.address;
-    var address = "abcasd str";
+    var address = context.watch<UserProvider>().user.address;
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(50),
@@ -65,15 +107,15 @@ class _AddressScreenState extends State<AddressScreen> {
                           style: const TextStyle(fontSize: 18),
                         ),
                       ),
-                    )
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      "OR",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    const SizedBox(height: 20),
                   ],
                 ),
-              const SizedBox(height: 20),
-              const Text(
-                "OR",
-                style: TextStyle(fontSize: 18),
-              ),
-              const SizedBox(height: 20),
               Form(
                 key: _addressFormKey,
                 child: Column(
@@ -100,6 +142,35 @@ class _AddressScreenState extends State<AddressScreen> {
                   ],
                 ),
               ),
+              const SizedBox(height: 10),
+              ApplePayButton(
+                margin: const EdgeInsets.only(top: 15),
+                width: double.infinity,
+                height: 50,
+                style: ApplePayButtonStyle.whiteOutline,
+                type: ApplePayButtonType.buy,
+                loadingIndicator: const Center(
+                  child: CircularProgressIndicator(),
+                ),
+                paymentConfigurationAsset: "applepay.json",
+                onPaymentResult: onApplePayResult,
+                paymentItems: _paymentItems,
+                onPressed: () => payPressed(address),
+              ),
+              const SizedBox(height: 10),
+              GooglePayButton(
+                margin: const EdgeInsets.only(top: 15),
+                width: double.infinity,
+                height: 50,
+                type: GooglePayButtonType.buy,
+                loadingIndicator: const Center(
+                  child: CircularProgressIndicator(),
+                ),
+                paymentConfigurationAsset: "gpay.json",
+                onPaymentResult: onGooglePayResult,
+                paymentItems: _paymentItems,
+                onPressed: () => payPressed(address),
+              )
             ],
           ),
         ),
--- lib/features/cart/screens/cart_screen.dart
+++ lib/features/cart/screens/cart_screen.dart
@@ -22,13 +22,20 @@ class _CartScreenState extends State<CartScreen> {
     Navigator.pushNamed(context, SearchScreen.routeName, arguments: query);
   }
 
-  void navigateToAddressScreen() {
-    Navigator.pushNamed(context, AddressScreen.routeName);
+  void navigateToAddressScreen(int sum) {
+    Navigator.pushNamed(context, AddressScreen.routeName,
+        arguments: sum.toString());
   }
 
   @override
   Widget build(BuildContext context) {
     final user = context.watch<UserProvider>().user;
+
+    int sum = 0;
+    user.cart
+        .map((e) => sum += e["quantity"] * e["product"]["price"] as int)
+        .toList();
+
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(50),
@@ -112,7 +119,7 @@ class _CartScreenState extends State<CartScreen> {
             padding: const EdgeInsets.all(8.0),
             child: CustomButton(
               text: "Proceed to Buy (${user.cart.length} Items)",
-              onTap: navigateToAddressScreen,
+              onTap: () => navigateToAddressScreen(sum),
               color: Colors.yellow[600],
             ),
           ),
