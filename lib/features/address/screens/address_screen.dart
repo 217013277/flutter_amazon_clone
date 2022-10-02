47ae3fc41b54fc5658c89047079744c321c7168b
yiuyu <217013277@stu.vtc.edu.hk>
2022-10-02 11:10:28 +0800

    create get order function

--- lib/features/address/screens/address_screen.dart
+++ lib/features/address/screens/address_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_amazon_clone/common/widgets/custom_button.dart';
 import 'package:flutter_amazon_clone/constants/utils.dart';
